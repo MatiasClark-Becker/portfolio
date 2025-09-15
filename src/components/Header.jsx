230ae95f9c9932c045ba41a4ff40a3e21c4cbdc8
Matias <matias.clarkbecker@gmail.com>
2025-09-15 13:08:11 -0400

    removed dark mode toggle, for now

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,5 @@
-import { useEffect, useState } from "react";
-
 export default function Header() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  // On mount, check saved preference
-  useEffect(() => {
-    const prefersDark =
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches);
-
-    if (prefersDark) {
-      document.documentElement.classList.add("dark");
-      setDarkMode(true);
-    } else {
-      document.documentElement.classList.remove("dark");
-      setDarkMode(false);
-    }
-  }, []);
-
-  // Toggle dark mode
-  const toggleDarkMode = () => {
-    if (darkMode) {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "light";
-      setDarkMode(false);
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
-      setDarkMode(true);
-    }
-  };
+  
   
   return (
     <header className="z-50 fixed top-0 left-0 right-0 py-6 sm:py-8 lg:py-12 bg-transparent">
@@ -45,20 +14,6 @@ export default function Header() {
           <a href="#about-me" className="font-sans text-white dark:text-[rgba(255,255,255,1)] duration-500 hover:text-gray-500">About</a>
           <a href="#experience" className="font-sans text-white dark:text-[rgba(255,255,255,1)] duration-500 hover:text-gray-500">Experience</a>
           <a href="#projects" className="font-sans text-white dark:text-[rgba(255,255,255,1)] duration-500 hover:text-gray-500">Projects</a>
-          
-          <div
-        onClick={toggleDarkMode}
-        className={`w-14 h-8 flex items-center rounded-full p-1 cursor-pointer transition-colors duration-500 ${
-          darkMode ? "bg-gray-700" : "bg-gray-300"
-        }`}
-      >
-        <div
-          className={`bg-white w-6 h-6 rounded-full shadow-md transform transition-transform duration-500 ${
-            darkMode ? "translate-x-6" : "translate-x-0"
-          }`}
-        /> 
-      
-      </div>
         </nav>
       </div>
     </header>
